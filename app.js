f5f400d3fea10413c732a045c82221a4c04a29df
cbenitez10 <cbenitezdiaz@gmail.com>
2018-06-19 23:12:36 -0400

    por hacer

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const { argv } = require('./config/yargs');
-
-const { crear } = require('./por-hacer/por-hacer');
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
+const porHacer = require('./por-hacer/por-hacer');
 
 let comando = argv._[0];
 
 switch ( comando ) {
     case 'crear':
-        let creado = crear( argv.descripcion );
+        let creado = porHacer.crear( argv.descripcion );
         console.log(creado);
         break;
     case 'listar':
-        console.log('Listar');
+        let listado = porHacer.getListado();
+        for( let tarea of listado ){
+            console.log( '=========Por Hacer========='.green );
+            console.log( tarea.descripcion );
+            console.log( 'Estado:', colors.yellow( tarea.completado ) );
+            console.log( '==========================='.green );
+        }
         break;
     case 'actualizar':
         console.log('Actualizar');
--- por-hacer/por-hacer.js
+++ por-hacer/por-hacer.js
@@ -3,7 +3,31 @@ const colors = require('colors');
 
 let listadoPorHacer = [];
 
+const guardarDB = () => {
+
+	let data = JSON.stringify( listadoPorHacer );
+
+	fs.writeFile( 'db/data.json', data, ( err ) => {
+
+		if( err ) throw new Error( 'Ocurrio un error al guardar el archivo', err );
+
+	});
+	
+}
+
+const cargarDB = () => {
+
+	try{
+		listadoPorHacer = require('../db/data.json');
+	}catch( e ){
+		listadoPorHacer = [];
+	}
+
+}
+
 const crear = ( descripcion ) => {
+
+	cargarDB();
 
 	let porHacer = {
 		descripcion,
@@ -12,10 +36,20 @@ const crear = ( descripcion ) => {
 
 	listadoPorHacer.push( porHacer );
 
+	guardarDB();
+
 	return porHacer;
 
 }
 
+const getListado = () => {
+
+	cargarDB();
+
+	return listadoPorHacer;
+}
+
 module.exports = {
-	crear
+	crear,
+	getListado
 }
